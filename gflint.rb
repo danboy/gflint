9e2aad8f504d10d91cda5c824b12074faa91e890
Dan Nawara <dan@gullivar.(none)>
2010-03-23 10:50:40 -0500

    switched to streaming api

--- gflint.rb
+++ gflint.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'broach'
+require "uri"
+require 'twitter/json_stream'
+require 'json'
+require "httparty"
 
 @last_message = 'gflint'
 
@@ -8,40 +11,44 @@ def notify(header,msg,options = {})
   `notify-send #{options[:img]} #{options[:delay]} '#{header}' '#{msg}'`
 end
 
-def monitor_room(room)
-  
-  data = Broach.session.get("/room/#{room.id.to_i}/transcript")
-  if @last_message == 'gflint'
-    @last_message = data["messages"].last
-  end
-  last_index = data['messages'].index(@last_message)
-  msgs = data['messages'].delete_if{|a| data['messages'].index(a) < last_index}
-  
-  if @last_message != data["messages"].last
-    msgs.each do |msg|
-      user = Broach.session.get("/users/#{msg['user_id']}")
-      notify(user.first[1]['name'], msg["body"])
+def monitor_room(options,room_name,room_uri)
+
+  EventMachine::run do
+    stream = Twitter::JSONStream.connect(options)
+    #users =  Connection.get(room_uri)
+    #puts users.inspect
+    stream.each_item do |item|
+      msg = JSON.parse(item)
+      notify( "#{room_name}:", msg["body"].to_s ) unless msg["body"].nil?
+    end
+    
+    stream.on_error do |message|
+      notify "ERROR: ", "#{message.inspect}"
+    end
+   
+    stream.on_max_reconnects do |timeout, retries|
+      puts "Tried #{retries} times to connect."
+      exit
     end
   end
   
-  @last_message = data["messages"].last
-  sleep(5)
-  monitor_room(room)
 end
 
 config = YAML::load_file("#{ENV['HOME']}/.gflintrc")
-threads = []
+
 config.keys.each do |account_name|
   rooms = config[account_name][:rooms]
   if rooms && rooms.size > 0
-    Broach.settings = { 'account' => account_name, 'token' => config[account_name][:token],'use_ssl'=>config[account_name][:ssl]}
     rooms.keys.each do |room_name|
-      room = Broach::Room.find_by_name(room_name)
-      threads << Thread.new(room) do |r|
-          monitor_room(r)
-      end
+      room_uri =  "http://#{account_name}.campfirenow.com/room/#{rooms[room_name][:id]}.json"
+      options = {
+        :path => "/room/#{rooms[room_name][:id]}/live.json",
+        :host => 'streaming.campfirenow.com',
+        :auth => "#{config[account_name][:token]}:x"
+      }
+      
+      #puts room.inspect
+      monitor_room(options,room_name,room_uri)
     end
   end
 end
-
-threads.each { |t| t.join }
